00773b976ec21c5a7033f2da5f4d5823d5ea0ce4
Danyyl <69661889+web-review@users.noreply.github.com>
2021-02-11 19:18:51 +0200

    Update AudioBotConfig.java

--- src/main/java/com/example/demo/web/config/bot/AudioBotConfig.java
+++ src/main/java/com/example/demo/web/config/bot/AudioBotConfig.java
@@ -2,7 +2,7 @@ package com.example.demo.web.config.bot;
 
 public class AudioBotConfig {
     private final static String BOT_NAME = "@audiox_bot";
-    private final static String TOKEN = "1657164999:AAFpuWs7Fxwal8R5sDF1hzUZ8fYDAtSWFLE";
+    private final static String TOKEN = "";
 
     public static String getBotName() {
         return BOT_NAME;
